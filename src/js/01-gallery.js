31ff2357bac3aef4661ee951264eae7734a276a3
Oleg Adamov <olegadamov84@gmail.com>
2022-08-19 14:23:56 +0300

    Package-json

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -1,5 +1,56 @@
-// Add imports above this line
-import { galleryItems } from './gallery-items';
+import { galleryItems } from './gallery-items.js';
 // Change code below this line
 
-console.log(galleryItems);
+const onGalleryContainer = document.querySelector('.gallery');
+const onItemsGallery = createItemsGallery(galleryItems);
+
+onGalleryContainer.insertAdjacentHTML('beforeend', onItemsGallery);
+
+function createItemsGallery(galleryItems) { 
+    return galleryItems.map(({ preview, original, description }) => {
+        return `
+                    <div class="gallery__item">
+            <a class="gallery__link" href="${original}">
+            <img
+                class="gallery__image"
+                src="${preview}"
+                data-source="${original}"
+                alt="${description}"
+                />
+                </a>
+        </div>
+        `
+    }).join('');
+}
+
+onGalleryContainer.addEventListener('click', onItemsGalleryItemClick);
+
+function onItemsGalleryItemClick(event) {
+    event.preventDefault();
+    if (event.target.classList.contains("gallery")) return;
+    const source = event.target.dataset.source;
+   
+    const instance = basicLightbox.create(`<img src="${source}">`, {
+
+	closable: true,
+
+	className: '',
+
+        onShow: (instance) => {
+            document.addEventListener('keydown', keyPress)
+    },
+
+        onClose: (instance) => {
+            document.removeEventListener('keydown', keyPress);
+    }
+});
+    instance.show();
+
+    function keyPress(event) {
+        if (event.key === "Escape") {
+            instance.close();
+        }
+    }
+
+    }
+
