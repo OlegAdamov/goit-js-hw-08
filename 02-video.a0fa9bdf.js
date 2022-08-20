a40425fdb66baf7e64dbe9b78400b00174842a72
OlegAdamov <103196379+OlegAdamov@users.noreply.github.com>
2022-08-20 15:49:34 +0000

    Deploying to gh-pages from @ OlegAdamov/goit-js-hw-08@ef62af020068dd1caee0bcc09858be786e5a4f1c 🚀

---
+++ 02-video.a0fa9bdf.js
@@ -0,0 +1,2 @@
+localStorage.setItem("videoplayer-current-time",JSON.stringify());const e=localStorage.getItem("videoplayer-current-time");JSOM.parse(e);
+//# sourceMappingURL=02-video.a0fa9bdf.js.map
