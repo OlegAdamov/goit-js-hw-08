ef62af020068dd1caee0bcc09858be786e5a4f1c
Oleg Adamov <olegadamov84@gmail.com>
2022-08-20 18:49:07 +0300

    Second task - process

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,1 +1,8 @@
 
+localStorage.setItem('videoplayer-current-time', JSON.stringify());
+
+const saveTime = localStorage.getItem('videoplayer-current-time');
+const parsedTime = JSOM.parse(saveTime);
+
+
+
