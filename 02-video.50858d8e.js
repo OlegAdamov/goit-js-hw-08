546e494a3c7122499791b620aeb228678bd164e7
OlegAdamov <103196379+OlegAdamov@users.noreply.github.com>
2022-08-21 10:41:32 +0000

    Deploying to gh-pages from @ OlegAdamov/goit-js-hw-08@c3a9be39364a7a87876f9f70de6fb5ff6870938f 🚀

--- 02-video.50858d8e.js
+++
@@ -1,2 +0,0 @@
-!function(){localStorage.setItem("videoplayer-current-time",JSON.stringify());var e=localStorage.getItem("videoplayer-current-time");JSOM.parse(e)}();
-//# sourceMappingURL=02-video.50858d8e.js.map
--- 02-video.a0fa9bdf.js
+++
@@ -1,2 +0,0 @@
-localStorage.setItem("videoplayer-current-time",JSON.stringify());const e=localStorage.getItem("videoplayer-current-time");JSOM.parse(e);
-//# sourceMappingURL=02-video.a0fa9bdf.js.map
