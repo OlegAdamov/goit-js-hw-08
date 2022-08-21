c3a9be39364a7a87876f9f70de6fb5ff6870938f
Oleg Adamov <olegadamov84@gmail.com>
2022-08-21 13:40:58 +0300

    Task-2 completed

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,8 +1,29 @@
+import Player from '@vimeo/player';
+import throttle from 'lodash.throttle';
+const STORAGE_KEY = "videoplayer-current-time";
+const iframe = document.querySelector('iframe');
 
-localStorage.setItem('videoplayer-current-time', JSON.stringify());
-
-const saveTime = localStorage.getItem('videoplayer-current-time');
-const parsedTime = JSOM.parse(saveTime);
+const player = new Player(iframe);
 
 
+const onPlay = function (data) {
+    localStorage.setItem(STORAGE_KEY, JSON.stringify(data.seconds))
+    // data is an object containing properties specific to that event
+};
 
+player.on('timeupdate', throttle(onPlay, 1000));
+      const saveTime = JSON.parse(localStorage.getItem(STORAGE_KEY));
+
+player.setCurrentTime(saveTime).then(function (seconds) {
+    
+}).catch(function(error) {
+    switch (error.name) {
+        case 'RangeError':
+            // the time was less than 0 or greater than the video’s duration
+            break;
+
+        default:
+            // some other error occurred
+            break;
+    }
+});
